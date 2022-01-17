25f60b029cb333bc13c747aba32e9b176859f235
justinevanessa <97602133+justinevanessa@users.noreply.github.com>
2022-01-17 09:21:33 +0100

    Wertzuweisung

--- script_01.js
+++ script_01.js
@@ -1,4 +1,15 @@
 
 /* Test */
 
+
 console.log("Hello World!");
+
+let firstName;
+firstName = "Max";
+console.log(firstName);
+
+
+let lastName = "Mütze";
+console.log(lastName);
+
+console.log (firstName+lastName);
